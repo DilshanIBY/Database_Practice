b25239e9aff4897e2bcafb15365b5a3a765c3e19
DilshanIBY <dilshaniru@gmail.com>
2024-04-29 03:53:13 +0530

    Add project files.

---
+++ Database_Practice/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Database_Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblQty = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(130, 389);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(138, 36);
+            this.btnAdd.TabIndex = 23;
+            this.btnAdd.Text = "Add Data";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(274, 389);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(138, 36);
+            this.btnDelete.TabIndex = 22;
+            this.btnDelete.Text = "Delete Data";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(418, 389);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(138, 36);
+            this.btnUpdate.TabIndex = 21;
+            this.btnUpdate.Text = "Update Data";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(533, 77);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(138, 36);
+            this.btnEnter.TabIndex = 20;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(225, 325);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(38, 16);
+            this.lblPrice.TabIndex = 19;
+            this.lblPrice.Text = "Price";
+            // 
+            // lblQty
+            // 
+            this.lblQty.AutoSize = true;
+            this.lblQty.Location = new System.Drawing.Point(225, 254);
+            this.lblQty.Name = "lblQty";
+            this.lblQty.Size = new System.Drawing.Size(55, 16);
+            this.lblQty.TabIndex = 18;
+            this.lblQty.Text = "Quantity";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(225, 191);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(44, 16);
+            this.lblName.TabIndex = 17;
+            this.lblName.Text = "Name";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(529, 29);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(20, 16);
+            this.lblId.TabIndex = 16;
+            this.lblId.Text = "ID";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(312, 322);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(188, 22);
+            this.txtPrice.TabIndex = 15;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(312, 188);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(188, 22);
+            this.txtName.TabIndex = 14;
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(312, 251);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(188, 22);
+            this.txtQty.TabIndex = 13;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(580, 26);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(91, 22);
+            this.txtId.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(719, 469);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblQty);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtQty);
+            this.Controls.Add(this.txtId);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblQty;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.TextBox txtId;
+    }
+}
+
---
+++ Database_Practice/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Xml.Linq;
+using System.IO;
+
+namespace Database_Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }        // Getting the Connection string
+        public static string getConnStr()
+        {
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string projectRootDirectory = Directory.GetParent(Directory.GetParent(currentDirectory).Parent.FullName).FullName;
+            string connectionString = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={projectRootDirectory}\Database1.mdf;Integrated Security=True";
+            return connectionString;
+        }
+        // Creating the Connection string object
+        SqlConnection conn = new SqlConnection(getConnStr());
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            // Declaring variables to store data
+            string id = txtId.Text;
+
+            // Creating the Sql Query
+            string query = $"SELECT * FROM goods WHERE Id='{id}';";
+
+            // Creating Sql Command
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            // Executing the Sql cammand
+            try
+            {
+                conn.Open();
+                SqlDataReader data = cmd.ExecuteReader();
+
+                if (data.HasRows)
+                {
+                    data.Read();
+                    txtName.Text = data["Item"].ToString();
+                    txtQty.Text = data["Qty"].ToString();
+                    txtPrice.Text = data["Price"].ToString();
+                }
+                conn.Close();
+
+                MessageBox.Show("SQL Command Excution Successful", "SUCCESS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Declaring variables to store data
+            string id = txtId.Text;
+            string name = txtName.Text;
+            string qty = txtQty.Text;
+            string price = txtPrice.Text;
+
+            // Creating the Sql Query
+            string query = $"INSERT INTO goods VALUES ('{id}','{name}','{qty}','{price}');";
+
+            // Creating Sql Command
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            // Executing the Sql cammand
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                MessageBox.Show("SQL Command Excution Successful", "SUCCESS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // Declaring variables to store data
+            string id = txtId.Text;
+            string name = txtName.Text;
+            string qty = txtQty.Text;
+            string price = txtPrice.Text;
+
+            // Creating the Sql Query
+            string query = $"UPDATE goods SET Item='{name}',Qty='{qty}',Price='{price}' WHERE Id='{id}';";
+
+            // Creating Sql Command
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            // Executing the Sql cammand
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                MessageBox.Show("SQL Command Excution Successful", "SUCCESS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Declaring variables to store data
+            string id = txtId.Text;
+
+            // Creating the Sql Query
+            string query = $"DELETE FROM goods WHERE Id='{id}'";
+
+            // Creating Sql Command
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            // Executing the Sql cammand
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                MessageBox.Show("SQL Command Excution Successful", "SUCCESS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
